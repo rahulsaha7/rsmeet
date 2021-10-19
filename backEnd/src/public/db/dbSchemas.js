527c36bda14bb66053d952174d5c12d30c15845b
rahulsaha7 <forrahul7@gmail.com>
2021-10-20 00:33:07 +0530

    dbSchema file is added for schemas for different collections

---
+++ backEnd/src/public/db/dbSchemas.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose');
+
+
+//Schema for auth table 
+const regSchema = new mongoose.Schema({
+    _id:{type:String,required:true},
+    name:{type: String, required:true},
+    email:{type:String,required:true},
+    phone:{type: String,required:true},
+    password:{type:String,required:true},
+    date : {type: Number , default:Date.now()}
+},{collection:'Auth'});
+
+
+//Schema for finding email email in auth database 
+
+const userExists = new mongoose.Schema({
+    email:{type:String}
+});
+
+const register = mongoose.model('reg',regSchema);
+const dupUser = new mongoose.model('dup',userExists);
+
+
+module.exports.schemaA = {
+    register,
+    dupUser
+}
+

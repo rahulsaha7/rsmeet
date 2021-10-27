79629d464954acd331c46559362b3303b7702a4e
rahulsaha7 <forrahul7@gmail.com>
2021-10-28 00:11:31 +0530

    In index page registration router and email check route is added

--- backEnd/src/indexLogin.js
+++ backEnd/src/indexLogin.js
@@ -4,28 +4,103 @@ const bcrypt = require('bcrypt');
 const crypto = require('crypto');
 const Router = express.Router();
 const reg = require('./public/components/Reg');
+const EmailCheck = require('./public/components/emailCheck');
+const multer = require('multer');
 
 
-Router.post('/',(req,res)=>{
-    
-   //If login is called then login will be called
-  
+const imageStorage = multer.diskStorage({
+  // Destination to store image     
+  destination: './src/public/images',
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + '_' + Date.now()
+      + (file.originalname))
+    // file.fieldname is name of the field (image)
+    // path.extname get the uploaded file extension
+  }
 });
 
-Router.post('/r',(req,res)=>{
-    //If reg is called then reg will be called 
-    let name = req.body.name;
-    let email = req.body.email;
-    let phone = req.body.phone;
-    let password = req.body.password;
-    reg.regis(name,email,phone,password).then((output)=>{
-        if(output['_message'])
-            res.send(output['_message']);
-        else
-        res.send("Registration Successfull");
+
+
+const imageUpload = multer({
+  storage: imageStorage,
+  limits: {
+    fileSize: 541435 // 1000000 Bytes = 1 MB
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(png|jpg|jpeg)$/)) {
+      // upload only png and jpg format
+      return cb(new Error('Please upload a Image'))
+    }
+    cb(undefined, true)
+  }
+})
+
+
+
+
+Router.post('/', (req, res) => {
+
+  //If login is called then login will be called
+
+});
+
+Router.post('/r',imageUpload.none(),(req, res) => {
+
+
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+
+
+
+  //imageUpload.single('image')
+
+  //this will give me a proper data 
+
+  let data = JSON.parse(JSON.stringify(req.body));
+
+
+  //now question arrises that whether should I add all at once or
+
+
+  //
+
+  console.log(data)
+  reg.regis(data.name, data.email, data.phone, data.password).then((output) => {
+    //Before sending output I have to create a user folder with username 
+    //And make sure that user name is unique
+    res.json(output);
+
+  }).catch((error) => {
+    res.json({
+      'error': true,
+      'Emessage': error.message
     })
-  
-    
+  })
+
+
+
+
 });
+
+
+
+Router.post('/check', imageUpload.none(), (req, res) => {
+  let data = JSON.parse(JSON.stringify(req.body));
+
+
+  EmailCheck.Echeck(data.email).then((output) => {
+    res.json(output);
+  }).catch((error) => {
+    res.json({
+      'error': true,
+      'Emessage': error.message
+    })
+  })
+});
+
 
 module.exports = Router;
